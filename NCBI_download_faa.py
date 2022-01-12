4ed166c968cd7a50f9719f3e1ca85d321685f761
Giovanna Gariglio <gigariglioc@gmail.com>
2022-01-12 15:15:14 -0300

    Update NCBI_download_faa.py
    Fixed connection issues.
    Fixed existing file error.

--- NCBI_download_faa.py
+++ NCBI_download_faa.py
@@ -1,13 +1,9 @@
 from ftplib import FTP
-import sys, os
+import sys, os, shutil
+#import gzip
 
 """
 This program downloads FASTA aminoacid (faa) files for user specified bacteria from NCBI's FTP server.
-
-When trying to download large amounts of files, a connection error has been ocurring.
-For this reason, I expect to make an updated version using rsync on Unix, currently recommended by NCBI 
-(https://www.ncbi.nlm.nih.gov/genome/doc/ftpfaq/#symlinks) for large data sets, which could solve the problem.
-I intend to do it by getting the names from the list of strains and closing the FTP connection to avoid having the host computer kill it.
 """
 
 if len(sys.argv) > 1: # checks if the user has called the species name as a system argument
@@ -22,7 +18,7 @@ while " " in bacteria: # checks if the argument was properly passed
 os_cwd = os.getcwd() # get os current working directory
 
 print("Connecting to NCBI's FTP server...")
-ftp = FTP('ftp.ncbi.nlm.nih.gov') # connect to host, default port
+ftp = FTP('ftp.ncbi.nlm.nih.gov', timeout=900) # connect to host, default port
 ftp.login() # user anonymous, passwd anonymous@
 
 print("Connected.")
@@ -39,26 +35,43 @@ ftp_origin = ftp.pwd() # here, ftp_origin means the folder in which all availabl
 
 print("Success. Getting strains...")
 list_of_strains = ftp.nlst() # creates a list with all available strains in the directory
+print("Strain list retrieved.")
 
 # the following code creates an OS directory to store downloaded faa files 
 protein_folder = bacteria + "_faa"
 print("Creating bacteria faa directory (" + protein_folder +") in current OS directory...")
-os.mkdir(protein_folder)
-print("Folder successfully created. Starting downloads...")
-os.chdir(protein_folder)
+protein_dir = protein_folder
 
-for strain in list_of_strains:
+attempts = 0
+while True:
+	try:
+		os.mkdir(protein_dir)
+		break
+	except FileExistsError:
+		attempts = attempts + 1
+		protein_dir = protein_folder + "_" + str(attempts)
+
+print("Folder successfully created. Starting downloads...")
+print()
+os.chdir(protein_dir)
+
+for dir_name in list_of_strains:
 	# some directories might have other files or directories other than the strain directories, which inevitably appear on the created list 
-	if "GCF" in strain: # allows only strain directories to be considered
-		try:
-			print("Downloading " + strain + " faa file...")
-			ftp.cwd(ftp_origin + "/" + strain) # changes to strain's directory
-			filename = strain + "_protein.faa.gz" # sets target file name
-			with open(filename, "wb") as f: # opens and closes target file
-				ftp.retrbinary('RETR '+ filename, f.write) # downloads target file while open
-			print("File successfully downloaded.")
-		except Exception as e: # ensures the code keeps running even if a strain doesn't have an available faa file
-			print("FASTA file for " + strain + " not extracted: an error occurred. Reason:")
-			"""
-			print(e)
+	if "GCF" not in dir_name: # allows for only strain directories to be considered
+		list_of_strains.remove(dir_name)
+
+for counter, strain in enumerate(list_of_strains):
+	try:
+		print("Downloading " + strain + " faa file...")
+		ftp.cwd(ftp_origin + "/" + strain) # changes to strain's directory
+		filename = strain + "_protein.faa.gz" # sets target file name
+		with open(filename, "wb") as f: # opens and closes target file
+			ftp.retrbinary('RETR '+ filename, f.write) # downloads target file while open
+		print("File " + str(counter + 1) + " out of " + str(len(list_of_strains)) + " successfully downloaded.")
+		print()
+	except Exception as e: # ensures the code keeps running even if a strain doesn't have an available faa file
+		print("FASTA file for " + strain + " not extracted: an error occurred. Reason:")
+		print(str(e))
+		print()
+
 print("Downloads completed.")
